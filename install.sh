3a234e3b8528595c6e4c050a2c3e70aeabecda78
En04ik <82300094+En04ik@users.noreply.github.com>
2021-07-27 16:41:03 +0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd $HOME/Update-All
+chmod +x 'update-all'
+cd $HOME/.local/share/applications
+touch update-all.desktop
+echo "[Desktop Entry]
+Name=Update All
+GenericName=Update helper
+Comment= UPDATE ALL is a bash script that update and upgrade repositories and packages on linux.
+Exec=/etc/terminal-run /home/angel/Update-All/update-all 'Update All'
+Icon=/home/angel/Update-All/icons/icon-64.png
+Terminal=false
+StartupNotify=true
+Type=Application
+Categories=Utility;
+" >> update-all.desktop || error "Failed to create menu button!"
