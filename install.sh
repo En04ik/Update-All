b7aa814d9a635cc821d601984a32c07bc4a9bf02
En04ik <82300094+En04ik@users.noreply.github.com>
2021-07-27 18:17:03 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ echo "[Desktop Entry]
 Name=Update All
 GenericName=Update helper
 Comment= UPDATE ALL is a bash script that update and upgrade repositories and packages on linux.
-Exec=/etc/terminal-run /home/angel/Update-All/update-all 'Update All'
-Icon=/home/angel/Update-All/icons/icon-64.png
+Exec=/etc/terminal-run $HOME/Update-All/update-all 'Update All'
+Icon=$HOME/Update-All/icons/icon-64.png
 Terminal=false
 StartupNotify=true
 Type=Application
