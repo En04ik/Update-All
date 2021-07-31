3f2cec1f5c818c008e8bf74893a40342f7cd27fb
En04ik <82300094+En04ik@users.noreply.github.com>
2021-07-31 13:51:43 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,4 @@
 
 cd $HOME/Update-All
 chmod +x 'update-all'
-cd $HOME/.local/share/applications
-touch update-all.desktop
-echo "[Desktop Entry]
-Name=Update All
-GenericName=Update helper
-Comment= UPDATE ALL is a bash script that update and upgrade repositories and packages on linux.
-Exec=/etc/terminal-run $HOME/Update-All/update-all 'Update All'
-Icon=$HOME/Update-All/icons/icon-64.png
-Terminal=false
-StartupNotify=true
-Type=Application
-Categories=Utility;
-" >> update-all.desktop || error "Failed to create menu button!"
+cp update-all $PREFIX/bin || error "Failed to create menu button!"
